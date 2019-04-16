c266dd8ad906c2570692316c0af15a1a69cc7a3a
Yosua Candra <yosuacandra37@yahoo.com>
2019-04-16 18:27:37 +0700

    Initial Commit

---
+++ Challenge2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge2
+//
+//  Created by Yosua Candra  on 16/04/19.
+//  Copyright © 2019 Yosua Candra . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Challenge2Tests/Challenge2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Challenge2Tests.swift
+//  Challenge2Tests
+//
+//  Created by Yosua Candra  on 16/04/19.
+//  Copyright © 2019 Yosua Candra . All rights reserved.
+//
+
+import XCTest
+@testable import Challenge2
+
+class Challenge2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
