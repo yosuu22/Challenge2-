1eb159c67769e2d90069b9fffbc655b3849a4865
Yosua Candra <yosuacandra37@yahoo.com>
2019-05-01 17:10:47 +0700

    Update iMessage
    only on real device.. Can't run on simulator

--- Challenge2/homeViewController.swift
+++ Challenge2/homeViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 import AVFoundation
 import AVKit
 import YoutubePlayer_in_WKWebView
+import MessageUI
 
-class homeViewController: UIViewController {
+class homeViewController: UIViewController,MFMessageComposeViewControllerDelegate {
     
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var youtubePlayer: WKYTPlayerView!
     @IBOutlet weak var collectionViewImage: UICollectionView!
     @IBOutlet weak var buttonDetail: UIButton!
     
+    @IBOutlet weak var buttonMessage: UIButton!
+    @IBOutlet weak var buttonInstagram: UIButton!
     var avPlayer: AVPlayer!
     var currentIndex = 0
     
@@ -30,6 +33,35 @@ class homeViewController: UIViewController {
         youtubePlayer.load(withVideoId: "gu9atqJ29P0")
         
     }
+    
+    
+    @IBAction func buttonKirimPesan(_ sender: Any) {
+        
+        let messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "Hi,Ivan ";
+        messageVC.recipients = ["082157063457"]
+        messageVC.messageComposeDelegate = self
+        
+        self.present(messageVC, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func buttonInstagramPress(_ sender: Any) {
+        let Username =  "ivanaldinataa" // Your Instagram Username here
+        let appURL = URL(string: "instagram://user?username=\(Username)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL) {
+            application.open(appURL)
+        } else {
+            // if Instagram app is not installed, open URL inside Safari
+            let webURL = URL(string: "https://instagram.com/\(Username)")!
+            application.open(webURL)
+        }
+    }
+    
+    
 }
 
 extension homeViewController: UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
@@ -55,5 +87,21 @@ extension homeViewController: UICollectionViewDelegate, UICollectionViewDataSour
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
        currentIndex = Int(scrollView.contentOffset.x / collectionViewImage.frame.size.width)
         pageControl.currentPage = currentIndex
+    }
+    // Protocol iMessage
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch (result) {
+        case .cancelled:
+            print("Message was cancelled")
+            dismiss(animated: true, completion: nil)
+        case .failed:
+            print("Message failed")
+            dismiss(animated: true, completion: nil)
+        case .sent:
+            print("Message was sent")
+            dismiss(animated: true, completion: nil)
+        default:
+            break
+        }
     }
 }
