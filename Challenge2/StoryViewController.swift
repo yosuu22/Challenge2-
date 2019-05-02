6d564c6e26f8b5e44a4fbf590b2914e9d365826d
Yosua Candra <yosuacandra37@yahoo.com>
2019-05-02 22:28:07 +0700

    Final Update Before Release 1.0

--- Challenge2/StoryViewController.swift
+++ Challenge2/StoryViewController.swift
@@ -13,10 +13,61 @@ class StoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
        
     }
     
 
-   
-
+    @IBAction func buttonFacebook(_ sender: Any) {
+        
+        let Username =  "van.aldinata" // Your Facebook Username here
+        let appURL = URL(string: "fb://profile/\(Username)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL) {
+            application.open(appURL)
+        } else {
+            // if Facebook app is not installed, open URL inside Safari
+            let webURL = URL(string: "https://facebook.com/\(Username)")!
+            application.open(webURL)
+        }
+    
+        
+    }
+    
+    
+    @IBAction func buttonLinkend(_ sender: Any) {
+        
+        let Username =  "ivan-aldinata-b76016137" // Your Facebook Username here
+        let appURL = URL(string: "linkedin://profile/\(Username)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL) {
+            application.open(appURL)
+        } else {
+            // if Facebook app is not installed, open URL inside Safari
+            let webURL = URL(string: "https://www.linkedin.com/in/\(Username)")!
+            application.open(webURL)
+        }
+        
+    }
+    
+    
+    @IBAction func buttonInstagram(_ sender: Any) {
+        
+        let Username =  "ivanaldinataa" // Your Instagram Username here
+        let appURL = URL(string: "instagram://user?username=\(Username)")!
+        let application = UIApplication.shared
+        
+        if application.canOpenURL(appURL) {
+            application.open(appURL)
+        } else {
+            // if Instagram app is not installed, open URL inside Safari
+            let webURL = URL(string: "https://instagram.com/\(Username)")!
+            application.open(webURL)
+        }
+        
+        
+    }
+    
 }
