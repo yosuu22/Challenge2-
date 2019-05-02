36352f2f623d7cd636b745a71d8fc0246a1a87a3
Yosua Candra <yosuacandra37@yahoo.com>
2019-05-02 11:50:10 +0700

    Fix Scrollview

---
+++ Challenge2/secondCellView.swift
@@ -0,0 +1,15 @@
+//
+//  secondCellView.swift
+//  Challenge2
+//
+//  Created by Yosua Candra  on 02/05/19.
+//  Copyright © 2019 Yosua Candra . All rights reserved.
+//
+
+import UIKit
+
+class secondCellView: UIView {
+
+   
+
+}
