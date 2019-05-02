fcd38ae1dc18481b65377e148ab34e38de34af70
Yosua Candra <yosuacandra37@yahoo.com>
2019-05-02 21:55:54 +0700

    Update Button di page story

---
+++ Challenge2/StoryViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StoryViewController.swift
+//  Challenge2
+//
+//  Created by Yosua Candra  on 02/05/19.
+//  Copyright © 2019 Yosua Candra . All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+
+}
